69fe1ff8acf4c4d7e28f9342933750559ab3ad76
Ole Marschik <ole.marschik@hhu.de>
2025-11-06 19:58:21 +0100

    ErrorHandler hinzugefügt für Fehlermeldungen bei zu großen Dateien

---
+++ src/main/java/com/awesome/thesis/ErrorHandler.java
@@ -0,0 +1,18 @@
+package com.awesome.thesis;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleException(MaxUploadSizeExceededException maxUploadSizeExceededExceptionException, Model model) {
+        model.addAttribute("maxUploadError","Maximale Dateigröße überschritten: max. 10MB");
+        return "upload";
+    }}
