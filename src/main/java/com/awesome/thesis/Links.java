97cfd95b22527be42ea0e2751438925accb39e05
Ryota Kariya <qet71seq@hhu.de>
2025-11-24 10:27:32 +0100

    Change Links-class to Record

--- src/main/java/com/awesome/thesis/Links.java
+++ src/main/java/com/awesome/thesis/Links.java
@@ -1,22 +1,4 @@
 package com.awesome.thesis;
 
-public class Links {
-    private String url;
-    private String text;
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getText() {
-        return text;
-    }
+public record Links(String url, String text) {
 }
