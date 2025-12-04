ce1506c32728abae96fa7cd7c86def880f2ac2f3
Janik Daub <daub.j@icloud.com>
2025-12-04 16:12:39 +0100

    Refactors Test 2 using mockStatic rather for static method verify

--- src/test/java/com/awesome/thesis/controller/DateiControllerTest.java
+++ src/test/java/com/awesome/thesis/controller/DateiControllerTest.java
@@ -3,16 +3,17 @@ package com.awesome.thesis.controller;
 import com.awesome.thesis.files.DateiTypPruefer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,10 +23,6 @@ class DateiControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
-    DateiTypPruefer dateiTypPruefer;
-
-
     @Test
     void get_auf_upload() throws Exception {
         mockMvc.perform(get("/upload"))
@@ -33,15 +30,16 @@ class DateiControllerTest {
                 .andExpect(view().name("upload"));
     }
 
-    @Disabled
     @Test
     void keine_DateiInfo_wird_erstellt_wenn_Datentyp_nicht_stimmt() throws Exception {
-        MockMultipartFile file = new MockMultipartFile("file", "test".getBytes());
-        when(DateiTypPruefer.verify(any())).thenReturn(false);
+        MockMultipartFile file = new MockMultipartFile("datei", "test".getBytes());
+        try(MockedStatic<DateiTypPruefer> mock = mockStatic(DateiTypPruefer.class)) {
+            mock.when(() -> DateiTypPruefer.verify(any())).thenReturn(false);
 
-        mockMvc.perform(multipart("/upload").file(file))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeDoesNotExist("dateiInfos"));
+            mockMvc.perform(multipart("/upload").file(file))
+                    .andExpect(status().isOk())
+                    .andExpect(model().attributeDoesNotExist("dateiInfos"));
+        }
     }
 
 }
