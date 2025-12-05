b238a722a930125ecf2902e66d9ff93c34121172
Ryota Kariya <qet71seq@hhu.de>
2025-12-05 12:51:35 +0100

    Change tests to fit with the new classes With this commit, the tests are able to run again. IDatabaseThema and FakeDatabaseThemaImpl are being used to simulate a database.

--- src/test/java/com/awesome/thesis/logic/application/service/themen/ThemaRepoTest.java
+++ src/test/java/com/awesome/thesis/logic/application/service/themen/ThemaRepoTest.java
@@ -1,6 +1,9 @@
 package com.awesome.thesis.logic.application.service.themen;
 
+import com.awesome.thesis.logic.domain.model.links.Link;
 import com.awesome.thesis.logic.domain.model.themen.Thema;
+import com.awesome.thesis.persistence.themen.FakeDatabaseThemaImpl;
+import com.awesome.thesis.persistence.themen.IDatabaseThema;
 import com.awesome.thesis.persistence.themen.ThemaRepoImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,22 +16,28 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ThemaRepoTest {
-//    @Test
-//    @DisplayName("If a Thema with the given title exists, it will be returned")
-//    void test_1() {
-//        Thema thema = mock(Thema.class);
-//        when(thema.getTitel()).thenReturn("titel");
-//        ThemaRepoImpl repository = new ThemaRepoImpl();
-//        repository.addThema(thema);
-//        assertThat(repository.getThema("titel")).isEqualTo(thema);
-//    }
-//    @Test
-//    @DisplayName("If no Thema with given titel exists, a NoSuchElementException gets thrown")
-//    void test_2() {
-//        Thema thema = mock(Thema.class);
-//        when(thema.getTitel()).thenReturn("titel");
-//        ThemaRepoImpl repository = new ThemaRepoImpl();
-//        repository.addThema(thema);
-//        assertThrows(NoSuchElementException.class, () -> repository.getThema("other titel"));
-//    }
+    @Test
+    @DisplayName("If a Thema with the given id exists, it will be returned")
+    void test_1() {
+
+        //Arrange
+        Thema thema = new Thema("Programmierpraktikum 2", "Beispieltext");
+        thema.addUrl(new Link("https://www.google.com/", "Google als Beispiel"));
+        thema.addUrl(new Link("https://www.youtube.com/", "Youtube als Beispiel"));
+        IDatabaseThema database = new FakeDatabaseThemaImpl();
+        String id = database.save(thema);
+
+        //Act
+        ThemaRepoImpl repository = new ThemaRepoImpl(database);
+
+        //Assert
+        assertThat(repository.getThema(id)).isEqualTo(thema);
+    }
+    @Test
+    @DisplayName("If no Thema with given id exists, a NoSuchElementException gets thrown")
+    void test_2() {
+        IDatabaseThema database = new FakeDatabaseThemaImpl();
+        ThemaRepoImpl repository = new ThemaRepoImpl(database);
+        assertThrows(NoSuchElementException.class, () -> repository.getThema("falsche id"));
+    }
 }
