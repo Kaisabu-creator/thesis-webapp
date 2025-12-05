ffe2060f12b53715b6ec6668faf555b0c39fff44
Ryota Kariya <qet71seq@hhu.de>
2025-12-05 11:41:31 +0100

    Adds impelemtation of IDatabaseThema with fake content

---
+++ src/main/java/com/awesome/thesis/persistence/themen/FakeDatabaseThemaImpl.java
@@ -0,0 +1,43 @@
+package com.awesome.thesis.persistence.themen;
+import com.awesome.thesis.logic.domain.model.links.Link;
+import com.awesome.thesis.logic.domain.model.themen.Thema;
+
+import java.util.*;
+
+public class FakeDatabaseThemaImpl implements IDatabaseThema{
+    Map<String, Thema> map = new HashMap<>();
+
+    public FakeDatabaseThemaImpl() {
+        Thema thema = new Thema("Programmierpraktikum 2", "Beispieltext");
+        thema.addUrl(new Link("https://www.google.com/", "Google als Beispiel"));
+        thema.addUrl(new Link("https://www.youtube.com/", "Youtube als Beispiel"));
+        save(thema);
+    }
+    public Thema get(String id) {
+        return map.get(id);
+    }
+
+    public String save(Thema thema) {
+        String id = UUID.randomUUID().toString();
+        System.out.println("Saving " + id);
+        map.put(id, thema);
+        return id;
+    }
+
+    public boolean containsKey(String id) {
+        return map.containsKey(id);
+    }
+
+    public void update(String id, Thema thema) {
+        map.put(id, thema);
+    }
+
+    public void delete(String id) {
+        map.remove(id);
+    }
+
+    public List<Thema> getAll() {
+        return new ArrayList<>(map.values());
+    }
+
+}
