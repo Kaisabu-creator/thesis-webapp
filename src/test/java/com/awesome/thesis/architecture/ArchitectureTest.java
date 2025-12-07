ab969ebb219c4f4e05b1da1ced0e7c38fc121302
Janik Daub <daub.j@icloud.com>
2025-12-07 15:11:25 +0100

    Adds disabled architecture test

---
+++ src/test/java/com/awesome/thesis/architecture/ArchitectureTest.java
@@ -0,0 +1,30 @@
+package com.awesome.thesis.architecture;
+
+import com.awesome.thesis.ThesisApplication;
+import com.sun.tools.javac.Main;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+
+public class ArchitectureTest {
+    private final JavaClasses klassen =
+            new ClassFileImporter().importPackagesOf(ThesisApplication.class);
+
+    @Test
+    @Disabled
+    @DisplayName("Die Mwst Anwendung hat eine Onion Architektur")
+    public void onionArchitektur() throws Exception {
+        ArchRule rule = onionArchitecture()
+                .domainModels("com.awesome.thesis.logic.domain.model..")
+                .domainServices("com.awesome.thesis.logic.domain.service..")
+                .applicationServices("com.awesome.thesis.logic.application.service..")
+                .adapter("web", "com.awesome.thesis.web")
+                .adapter("persistence", "com.awesome.thesis.persistence");
+        rule.check(klassen);
+    }
+}
