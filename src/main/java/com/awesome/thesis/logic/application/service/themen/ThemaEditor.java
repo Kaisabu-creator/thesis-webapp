d1780097ecfa77be14eb39717164c4c21cdb404a
Ryota Kariya <qet71seq@hhu.de>
2025-12-08 15:26:58 +0100

    Adds ThemaEditor and one test More tests will come shortly.

--- src/main/java/com/awesome/thesis/logic/application/service/themen/ThemaEditor.java
+++ src/main/java/com/awesome/thesis/logic/application/service/themen/ThemaEditor.java
@@ -1,4 +1,68 @@
 package com.awesome.thesis.logic.application.service.themen;
 
+import com.awesome.thesis.logic.domain.model.links.Link;
+import com.awesome.thesis.logic.domain.model.themen.Thema;
+
+import java.util.NoSuchElementException;
+
 public class ThemaEditor {
+    IThemaRepo repository;
+
+    public ThemaEditor(IThemaRepo repository) {
+        this.repository = repository;
+    }
+
+    public void addLink(String id, Link link) {
+        Thema thema = repository.get(id);
+        thema.addUrl(link);
+        repository.update(id,thema);
+    }
+
+    public void removeLink(String id, Link link) {
+        Thema thema = repository.get(id);
+        thema.removeUrl(link);
+        repository.update(id, thema);
+    }
+
+    public void editTitel(String id, String titel) {
+        Thema thema = repository.get(id);
+        thema.setTitel(titel);
+        repository.update(id, thema);
+    }
+
+    public void editBeschreibung(String id, String beschreibung) {
+        Thema thema = repository.get(id);
+        thema.setBeschreibung(beschreibung);
+        repository.update(id, thema);
+    }
+
+    public void deleteThema(String id) {
+        Thema thema = repository.get(id);
+        repository.delete(id);
+    }
+
+    public void addThema(Thema thema) {
+        if (thema.getId() != null) {
+            if (repository.containsKey(thema.getId())) {
+                repository.update(thema.getId(), thema);
+            }
+        } else {
+            repository.save(thema);
+        }
+    }
+
+    public Thema getThema(String id) {
+        if (repository.containsKey(id)) {
+            return repository.get(id);
+        } else {
+            throw new NoSuchElementException("Thema with id " + id + "not found");
+        }
+    }
+
+    //TODO: Jetzt noch Tests schreiben
+
+
+
+
+
 }
--- src/test/java/com/awesome/thesis/logic/application/service/themen/ThemaEditorTest.java
+++ src/test/java/com/awesome/thesis/logic/application/service/themen/ThemaEditorTest.java
@@ -1,4 +1,28 @@
 package com.awesome.thesis.logic.application.service.themen;
 
+import com.awesome.thesis.logic.domain.model.links.Link;
+import com.awesome.thesis.logic.domain.model.themen.Thema;
+import com.awesome.thesis.persistence.themen.IDatabaseThema;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ThemaEditorTest {
+    @Test
+    @DisplayName("When a new Link gets added, its saved in the repository")
+    void test_1() {
+        Thema thema = new Thema("Test", "Beschreibung");
+        IThemaRepo repo = mock(IThemaRepo.class);
+        when(repo.get("a")).thenReturn(thema);
+        ThemaEditor editor = new ThemaEditor(repo);
+        Link link = mock(Link.class);
+        editor.addLink("a", link);
+        assertThat(thema.getLinks()).contains(link);
+    }
+
+
 }
