f59fb52ac73cb204d28a85a235fd7d1874527f46
Janik Daub <daub.j@icloud.com>
2025-12-12 09:13:16 +0100

    Refactor jakarta Email already as to be NotNull

--- src/main/java/com/awesome/thesis/controller/dto/EmailKontaktDTO.java
+++ src/main/java/com/awesome/thesis/controller/dto/EmailKontaktDTO.java
@@ -1,7 +1,6 @@
 package com.awesome.thesis.controller.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 
-public record EmailKontaktDTO(String label, @NotNull @Email String wert) {
+public record EmailKontaktDTO(String label, @Email String wert) {
 }
