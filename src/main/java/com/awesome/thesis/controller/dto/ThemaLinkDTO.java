650e1d38381b4975fa8d307d31c4f02cdf2ea053
Ryota Kariya <qet71seq@hhu.de>
2025-12-14 19:39:02 +0100

    Add validation to check for valid url

--- src/main/java/com/awesome/thesis/controller/dto/ThemaLinkDTO.java
+++ src/main/java/com/awesome/thesis/controller/dto/ThemaLinkDTO.java
@@ -2,10 +2,11 @@ package com.awesome.thesis.controller.dto;
 
 import com.awesome.thesis.logic.domain.model.links.Link;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.List;
 
 public record ThemaLinkDTO(
-        @NotBlank(message = "Bitte das Feld nicht leer lassen") String url,
+        @NotBlank(message = "Bitte das Feld nicht leer lassen") @URL(message = "Bitte eine valide Url eingeben") String url,
         @NotBlank(message = "Bitte eine Beschreibung einfügen")String urlBeschreibung) {
 }
