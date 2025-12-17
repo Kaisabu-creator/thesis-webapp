d3bb889a2631334bc38df92f489f038305d3cbdf
Ole Marschik <ole.marschik@hhu.de>
2025-12-17 12:27:18 +0100

    environment variables added

--- src/main/java/com/awesome/thesis/logic/application/service/files/DateiTypPruefer.java
+++ src/main/java/com/awesome/thesis/logic/application/service/files/DateiTypPruefer.java
@@ -1,12 +1,8 @@
 package com.awesome.thesis.logic.application.service.files;
 
-import com.awesome.thesis.logic.domain.model.files.DateiInfos;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.Set;
 
-@Component
 public class DateiTypPruefer {
     private static final Set<String> ERLAUBTE_DATEIENDUNGEN = Set.of("zip", "pdf", "md");
 
