fea0e04e0efed8041da162162e2894b45afa2bcf
Janik Daub <daub.j@icloud.com>
2025-12-17 13:17:14 +0100

    Loads right profile out of databank

--- src/main/java/com/awesome/thesis/controller/betreuende/BetreuendeController.java
+++ src/main/java/com/awesome/thesis/controller/betreuende/BetreuendeController.java
@@ -1,7 +1,10 @@
 package com.awesome.thesis.controller.betreuende;
 
+import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
 import com.awesome.thesis.logic.domain.model.profil.Profil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Secured("ROLE_BETREUENDE")
 @RequestMapping("/betreuende")
 public class BetreuendeController {
+    @Autowired
+    ProfilEditor editor;
+
     @GetMapping("profil")
-    public String getProfil(Model model) {
-        model.addAttribute("profil", new Profil(""));
+    public String getProfil(Model model, OAuth2AuthenticationToken auth) {
+        Integer id = auth.getPrincipal().getAttribute("id");
+        model.addAttribute("profil", editor.get(String.valueOf(id)));
         return "betreuendeProfil";
     }
 }
