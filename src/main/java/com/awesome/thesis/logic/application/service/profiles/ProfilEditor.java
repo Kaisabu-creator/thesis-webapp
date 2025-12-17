8e6c00407bb98bff46f16b3a92a2aa0b1391f7b1
Janik Daub <daub.j@icloud.com>
2025-12-17 10:41:53 +0100

    Adds contains method

--- src/main/java/com/awesome/thesis/logic/application/service/profiles/ProfilEditor.java
+++ src/main/java/com/awesome/thesis/logic/application/service/profiles/ProfilEditor.java
@@ -56,6 +56,10 @@ public class ProfilEditor {
         throw new IllegalArgumentException("No such id " + id);
     }
 
+    public boolean contains(String id) {
+        return profile.containsKey(id);
+    }
+
     public List<Profil> getAll() {
         return profile.getAll();
     }
