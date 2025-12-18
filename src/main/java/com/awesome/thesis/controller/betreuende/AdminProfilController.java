1ec6387cb677479fbc3a082c8378a3e410d00179
Janik Daub <daub.j@icloud.com>
2025-12-18 12:34:08 +0100

    profilEdit for Betreuende

---
+++ src/main/java/com/awesome/thesis/controller/betreuende/AdminProfilController.java
@@ -0,0 +1,54 @@
+package com.awesome.thesis.controller.betreuende;
+
+import com.awesome.thesis.controller.dto.EmailKontaktDTO;
+import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
+import com.awesome.thesis.logic.domain.model.profil.Kontakt;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/betreuende")
+@Secured("ROLE_BETREUENDE")
+public class AdminProfilController {
+    @Autowired
+    ProfilEditor editor;
+
+    @GetMapping("profilEdit")
+    public String profilEdit(Model model, OAuth2AuthenticationToken auth) {
+        Integer id = auth.getPrincipal().getAttribute("id");
+        model.addAttribute("profil", editor.get(id));
+        model.addAttribute("email", new EmailKontaktDTO("", ""));
+        return "betreuende/profilEdit";
+    }
+
+    @PostMapping("profilEdit")
+    public String profilEdit(@ModelAttribute("name") String name, OAuth2AuthenticationToken auth) {
+        Integer id = auth.getPrincipal().getAttribute("id");
+        editor.editName(id, name);
+        return "redirect:/betreuende/profilEdit/";
+    }
+
+    @PostMapping("profilEdit/deleteKontakt")
+    public String deleteKontakt(@ModelAttribute Kontakt kontakt,OAuth2AuthenticationToken auth) {
+        Integer id = auth.getPrincipal().getAttribute("id");
+        editor.removeKontakt(id, kontakt);
+        return "redirect:/betreuende/profilEdit/";
+    }
+
+    @PostMapping("profilEdit/addEmail")
+    public String addEmail(@Valid @ModelAttribute("email") EmailKontaktDTO email, BindingResult result, Model model, OAuth2AuthenticationToken auth) {
+        Integer id = auth.getPrincipal().getAttribute("id");
+        if  (result.hasErrors()) {
+            model.addAttribute("profil", editor.get(id));
+            return "betreuende/profilEdit";
+        }
+        editor.addEmail(id, email.label(), email.wert());
+        return "redirect:/betreuende/profilEdit/";
+    }
+}
---
+++ src/test/java/com/awesome/thesis/controller/betreuende/AdminProfilControllerTest.java
@@ -0,0 +1,177 @@
+package com.awesome.thesis.controller.betreuende;
+
+import com.awesome.thesis.configurations.AppUserService;
+import com.awesome.thesis.configurations.MethodSecurityConfig;
+import com.awesome.thesis.configurations.SecurityConfig;
+import com.awesome.thesis.helper.WithMockOAuth2User;
+import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
+import com.awesome.thesis.logic.domain.model.profil.Kontakt;
+import com.awesome.thesis.logic.domain.model.profil.Kontaktart;
+import com.awesome.thesis.logic.domain.model.profil.Profil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import({SecurityConfig.class, MethodSecurityConfig.class, AppUserService.class})
+@WebMvcTest(AdminProfilController.class)
+class AdminProfilControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    ProfilEditor editor;
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("get auf /betreuende/profilEdit funktioniert")
+    void get_profilEdit() throws Exception {
+        Profil profil = new Profil(1, "test");
+        when(editor.get(anyLong())).thenReturn(profil);
+        mockMvc.perform(get("/betreuende/profilEdit"))
+                .andExpect(model().attribute("profil", profil))
+                .andExpect(view().name("betreuende/profilEdit"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockOAuth2User()
+    @DisplayName("get auf /betreuende/profilEdit funktioniert nicht ohne Rechte")
+    void get_profilEdit_withoutRights() throws Exception {
+        Profil profil = new Profil(1, "test");
+        when(editor.get(anyLong())).thenReturn(profil);
+        mockMvc.perform(get("/betreuende/profilEdit"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Name ändern funktioniert")
+    void post_Name() throws Exception {
+        mockMvc.perform(post("/betreuende/profilEdit")
+                        .param("name", "test")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Name ändern funktioniert")
+    void post_Name_BackEnd() throws Exception {
+        mockMvc.perform(post("/betreuende/profilEdit")
+                .param("name", "test")
+                .with(csrf()));
+        verify(editor).editName(1, "test");
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt löschen")
+    void post_deleteKontakt() throws Exception {
+        mockMvc.perform(post("/betreuende/profilEdit/deleteKontakt")
+                        .param("label", "test")
+                        .param("wert", "test@icloud.com")
+                        .param("kontaktart", Kontaktart.EMAIL.toString())
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt löschen")
+    void post_deleteKontakt_BackEnd() throws Exception {
+        mockMvc.perform(post("/betreuende/profilEdit/deleteKontakt")
+                .param("label", "test")
+                .param("wert", "test@icloud.com")
+                .param("kontaktart", Kontaktart.EMAIL.toString())
+                .with(csrf()));
+        verify(editor).removeKontakt(1, new Kontakt("test", "test@icloud.com", Kontaktart.EMAIL));
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt hinzufügen funktioniert")
+    void post_addKontakt() throws Exception {
+        mockMvc.perform(post("/betreuende/profilEdit/addEmail")
+                        .param("label", "test")
+                        .param("wert", "test@icloud.com")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt hinzufügen funktioniert")
+    void post_addKontakt_BackEnd() throws Exception {
+        mockMvc.perform(post("/betreuende/profilEdit/addEmail")
+                .param("label", "test")
+                .param("wert", "test@icloud.com")
+                .with(csrf()));
+        verify(editor).addEmail(1, "test", "test@icloud.com");
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt hinzufügen funktioniert nicht bei fehlender Email")
+    void post_addKontakt_keinWert() throws Exception {
+        Profil profil = new Profil(1, "test");
+        when(editor.get(anyLong())).thenReturn(profil);
+        mockMvc.perform(post("/betreuende/profilEdit/addEmail")
+                        .param("label", "test")
+                        .param("wert", "")
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt hinzufügen funktioniert nicht bei fehlender Email")
+    void post_addKontakt_BackEnd_keinWert() throws Exception {
+        Profil profil = new Profil(1, "test");
+        when(editor.get(anyLong())).thenReturn(profil);
+        mockMvc.perform(post("/betreuende/profilEdit/addEmail")
+                .param("label", "test")
+                .param("wert", "")
+                .with(csrf()));
+        verify(editor, never()).addEmail(anyLong(), any(), any());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt hinzufügen funktioniert nicht bei falscher Email")
+    void post_addKontakt_keineEmail() throws Exception {
+        Profil profil = new Profil(1, "test");
+        when(editor.get(anyLong())).thenReturn(profil);
+        mockMvc.perform(post("/betreuende/profilEdit/addEmail")
+                        .param("label", "test")
+                        .param("wert", "test")
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = {"BETREUENDE"}, id=1)
+    @DisplayName("post Kontakt hinzufügen funktioniert nicht bei falscher Email")
+    void post_addKontakt_BackEnd_keineEmail() throws Exception {
+        Profil profil = new Profil(1, "test");
+        when(editor.get(anyLong())).thenReturn(profil);
+        mockMvc.perform(post("/betreuende/profilEdit/addEmail")
+                .param("label", "test")
+                .param("wert", "test")
+                .with(csrf()));
+        verify(editor, never()).addEmail(anyLong(), any(), any());
+    }
+}
