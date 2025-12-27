63cdba56da197bed22143817ffac6e4ca2f55b45
Janik Daub <daub.j@icloud.com>
2025-12-27 11:45:42 +0100

    Refactor

--- src/main/java/com/awesome/thesis/controller/ProfilController.java
+++ src/main/java/com/awesome/thesis/controller/ProfilController.java
@@ -2,7 +2,6 @@ package com.awesome.thesis.controller;
 
 import com.awesome.thesis.logic.application.service.fachgebiete.FachgebieteEditor;
 import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
-import com.awesome.thesis.logic.domain.model.profil.Profil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
