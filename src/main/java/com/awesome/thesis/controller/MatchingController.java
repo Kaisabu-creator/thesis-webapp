9a4e951e88c64208eff93865cca8e826e7531c39
Janik Daub <daub.j@icloud.com>
2025-12-29 23:12:27 +0100

    Adds profile to matching

--- src/main/java/com/awesome/thesis/controller/MatchingController.java
+++ src/main/java/com/awesome/thesis/controller/MatchingController.java
@@ -1,12 +1,19 @@
 package com.awesome.thesis.controller;
 
+import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MatchingController {
+    @Autowired
+    ProfilEditor profilEditor;
+
     @GetMapping("/matching")
-    public String matching() throws Exception {
+    public String matching(Model model) throws Exception {
+        model.addAttribute("profile", profilEditor.getAll());
         return "matching";
     }
 }
