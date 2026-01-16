1da65671d6096bff601fb6aaab05e66c1946571b
Janik Daub <daub.j@icloud.com>
2026-01-16 11:59:18 +0100

    Adds methods Crud

--- src/main/java/com/awesome/thesis/persistence/profiles/ProfileDBRepository.java
+++ src/main/java/com/awesome/thesis/persistence/profiles/ProfileDBRepository.java
@@ -1,7 +1,13 @@
 package com.awesome.thesis.persistence.profiles;
 
-import org.springframework.context.annotation.Profile;
+import com.awesome.thesis.logic.domain.model.profil.Profil;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
+import java.util.List;
 
-public interface ProfileDBRepository extends CrudRepository<Profile, Integer> {
+public interface ProfileDBRepository extends CrudRepository<Profil, Integer> {
+    @NonNull
+    List<Profil> findAll();
+
+    Profil findById(int id);
 }
