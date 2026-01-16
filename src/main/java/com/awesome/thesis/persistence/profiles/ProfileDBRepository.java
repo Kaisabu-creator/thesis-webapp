316ca776f1f7acfe709c348fd8a094ea6adc567c
Janik Daub <daub.j@icloud.com>
2026-01-16 12:44:17 +0100

    Adds insert method for profil

--- src/main/java/com/awesome/thesis/persistence/profiles/ProfileDBRepository.java
+++ src/main/java/com/awesome/thesis/persistence/profiles/ProfileDBRepository.java
@@ -1,7 +1,10 @@
 package com.awesome.thesis.persistence.profiles;
 
 import com.awesome.thesis.logic.domain.model.profil.Profil;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import java.util.List;
 
@@ -10,4 +13,8 @@ public interface ProfileDBRepository extends CrudRepository<Profil, Integer> {
     List<Profil> findAll();
 
     Profil findById(int id);
+
+    @Modifying
+    @Query("insert into profil (id, name) values (:id, :name)")
+    void insert(@Param("id") int id, @Param("name") String name);
 }
