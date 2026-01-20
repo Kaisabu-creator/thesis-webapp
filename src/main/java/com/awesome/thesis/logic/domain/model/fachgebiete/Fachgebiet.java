c18fa90caacd2ab10e822cf8a9c701b2a0d96e06
Janik Daub <daub.j@icloud.com>
2026-01-20 15:01:23 +0100

    Fachgebiet aggregate follows google standards

--- src/main/java/com/awesome/thesis/logic/domain/model/fachgebiete/Fachgebiet.java
+++ src/main/java/com/awesome/thesis/logic/domain/model/fachgebiete/Fachgebiet.java
@@ -1,31 +1,35 @@
 package com.awesome.thesis.logic.domain.model.fachgebiete;
 
 import com.awesome.thesis.annotations.AggregateRoot;
-import org.springframework.data.annotation.Id;
-
 import java.util.Objects;
 
+/**
+ * Diese Klasse ist das Aggregat Root des Fachgebiet-Aggregates und
+ * speichert das Fachgebiet in einem String.
+ */
 @AggregateRoot
 public class Fachgebiet {
-    private final String name;
-
-    public Fachgebiet (String name) {
-        this.name = name;
+  private final String name;
+  
+  public Fachgebiet(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Fachgebiet that = (Fachgebiet) o;
-        return Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(name);
-    }
+    Fachgebiet that = (Fachgebiet) o;
+    return Objects.equals(name, that.name);
+  }
+  
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(name);
+  }
 }
