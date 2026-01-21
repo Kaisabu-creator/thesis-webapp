8fc2876ba6830c4a9ae03547e001d0a451958d9f
Janik Daub <daub.j@icloud.com>
2026-01-21 13:31:31 +0100

    controller.advice follows Google Standard

--- src/main/java/com/awesome/thesis/controller/advice/RollenControllerAdvice.java
+++ src/main/java/com/awesome/thesis/controller/advice/RollenControllerAdvice.java
@@ -6,22 +6,33 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+/**
+ * Controller Advice für alle Controller.
+ */
 @ControllerAdvice()
 public class RollenControllerAdvice {
-    @ModelAttribute("navButton")
-    public NavbarButtonDto getNavButton(Authentication token) {
-        if(!(token instanceof OAuth2AuthenticationToken auth)) {
-            return new NavbarButtonDto("Anmelden", "/login");
-        }
-        boolean isAdmin = auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
-        if (isAdmin) {
-            return new NavbarButtonDto("Admin", "/admin");
-        }
-        boolean isBetreuende = auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_BETREUENDE"));
-        String name = auth.getPrincipal().getAttribute("login");
-        if (isBetreuende) {
-            return new NavbarButtonDto(name, "/betreuende/profilEdit");
-        }
-        return null;
+  /**
+   * Methode die navButton vom Typ {@link NavbarButtonDto} zur Personalisierung hinzufügt.
+   *
+   * @param token {@link Authentication}
+   * @return {@link NavbarButtonDto}
+   */
+  @ModelAttribute("navButton")
+  public NavbarButtonDto getNavButton(Authentication token) {
+    if (!(token instanceof OAuth2AuthenticationToken auth)) {
+      return new NavbarButtonDto("Anmelden", "/login");
     }
+    boolean isAdmin = auth.getAuthorities().stream()
+        .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+    if (isAdmin) {
+      return new NavbarButtonDto("Admin", "/admin");
+    }
+    boolean isBetreuende = auth.getAuthorities().stream()
+        .anyMatch(a -> a.getAuthority().equals("ROLE_BETREUENDE"));
+    String name = auth.getPrincipal().getAttribute("login");
+    if (isBetreuende) {
+      return new NavbarButtonDto(name, "/betreuende/profilEdit");
+    }
+    return new NavbarButtonDto("Abmelden", "/logout");
+  }
 }
