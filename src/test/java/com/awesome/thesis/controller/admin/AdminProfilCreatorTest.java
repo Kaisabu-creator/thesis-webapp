1cb545f142e8373dcdf088550f585d38541def3b
Janik Daub <daub.j@icloud.com>
2026-01-21 14:16:46 +0100

    AdminProfilCreatorTest follows GoogleStandard

--- src/test/java/com/awesome/thesis/controller/admin/AdminProfilCreatorTest.java
+++ src/test/java/com/awesome/thesis/controller/admin/AdminProfilCreatorTest.java
@@ -1,10 +1,23 @@
 package com.awesome.thesis.controller.admin;
 
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import com.awesome.thesis.configurations.AppUserService;
 import com.awesome.thesis.configurations.MethodSecurityConfig;
 import com.awesome.thesis.configurations.SecurityConfig;
 import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
 import com.awesome.thesis.logic.domain.model.profil.Profil;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,112 +27,103 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
 @Import({SecurityConfig.class, MethodSecurityConfig.class, AppUserService.class})
 @WebMvcTest(AdminProfilCreator.class)
 class AdminProfilCreatorTest {
-    @Autowired
-    MockMvc mockMvc;
-
-    @MockitoBean
-    ProfilEditor editor;
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("Ein Admin kann die ProfilCreate Seite aufrufen")
-    void get_createProfil() throws Exception {
-        mockMvc.perform(get("/admin"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @WithMockUser()
-    @DisplayName("Ohne Admin-Rechte kann die ProfilCreate Seite nicht aufgerufen werden")
-    void get_createProfil_withoutRights() throws Exception {
-        mockMvc.perform(get("/admin"))
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("get gibt das richtige zurück")
-    void get_createProfil_model() throws Exception {
-        List<Profil> list = List.of();
-        when(editor.getAll()).thenReturn(list);
-        mockMvc.perform(get("/admin"))
-                .andExpect(model().attribute("profile", list))
+  @Autowired
+  MockMvc mockMvc;
+  
+  @MockitoBean
+  ProfilEditor editor;
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("Ein Admin kann die ProfilCreate Seite aufrufen")
+  void get_createProfil() throws Exception {
+    mockMvc.perform(get("/admin"))
+        .andExpect(status().isOk());
+  }
+  
+  @Test
+  @WithMockUser()
+  @DisplayName("Ohne Admin-Rechte kann die ProfilCreate Seite nicht aufgerufen werden")
+  void get_createProfil_withoutRights() throws Exception {
+    mockMvc.perform(get("/admin"))
+        .andExpect(status().isForbidden());
+  }
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("get gibt das richtige zurück")
+  void get_createProfil_model() throws Exception {
+    List<Profil> list = List.of();
+    when(editor.getAll()).thenReturn(list);
+    mockMvc.perform(get("/admin"))
+        .andExpect(model().attribute("profile", list))
         .andExpect(view().name("admin/profileAdmin"));
-    }
-
-    @Test
-    @WithMockUser()
-    @DisplayName("Ohne Admin-Rechte kann ein post nicht ausführen")
-    void post_createProfil_withoutRights() throws Exception {
-        mockMvc.perform(post("/admin/createBetreuende")
-                        .with(csrf()))
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("Ein Admin kann ein post auf profilCreate ausführen")
-    void post_createProfil() throws Exception {
-        mockMvc.perform(post("/admin/createBetreuende")
-                        .param("id", "1")
-                        .param("name", "test")
-                        .with(csrf()))
-                .andExpect(status().is3xxRedirection());
-    }
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("Ein Admin kann ein post auf profilCreate ausführen")
-    void post_createProfil_backEnd() throws Exception {
-        mockMvc.perform(post("/admin/createBetreuende")
-                        .param("id", "1")
-                        .param("name", "test")
-                        .with(csrf()));
-        verify(editor).create(1, "test");
-    }
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("id darf nicht leer sein")
-    void post_createProfil_backEnd_idCantBeEmpty() throws Exception {
-        mockMvc.perform(post("/admin")
-                .param("id", "")
-                .param("name", "test")
-                .with(csrf()));
-        verify(editor, never()).create(anyInt(), any());
-    }
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("id muss ein positiver int sein")
-    void post_createProfil_backEnd_idNeedsToBeInt() throws Exception {
-        mockMvc.perform(post("/admin")
-                .param("id", "0")
-                .param("name", "test")
-                .with(csrf()));
-        verify(editor, never()).create(anyInt(), any());
-    }
-
-    @Test
-    @WithMockUser(roles = {"ADMIN"})
-    @DisplayName("name darf nicht leer sein")
-    void post_createProfil_backEnd_NameCantBeEmpty() throws Exception {
-        mockMvc.perform(post("/admin")
-                .param("id", "1")
-                .param("name", "")
-                .with(csrf()));
-        verify(editor, never()).create(anyInt(), any());
-    }
+  }
+  
+  @Test
+  @WithMockUser()
+  @DisplayName("Ohne Admin-Rechte kann ein post nicht ausführen")
+  void post_createProfil_withoutRights() throws Exception {
+    mockMvc.perform(post("/admin/createBetreuende")
+            .with(csrf()))
+        .andExpect(status().isForbidden());
+  }
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("Ein Admin kann ein post auf profilCreate ausführen")
+  void post_createProfil() throws Exception {
+    mockMvc.perform(post("/admin/createBetreuende")
+            .param("id", "1")
+            .param("name", "test")
+            .with(csrf()))
+        .andExpect(status().is3xxRedirection());
+  }
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("Ein Admin kann ein post auf profilCreate ausführen")
+  void post_createProfil_backEnd() throws Exception {
+    mockMvc.perform(post("/admin/createBetreuende")
+        .param("id", "1")
+        .param("name", "test")
+        .with(csrf()));
+    verify(editor).create(1, "test");
+  }
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("id darf nicht leer sein")
+  void post_createProfil_backEnd_idCantBeEmpty() throws Exception {
+    mockMvc.perform(post("/admin")
+        .param("id", "")
+        .param("name", "test")
+        .with(csrf()));
+    verify(editor, never()).create(anyInt(), any());
+  }
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("id muss ein positiver int sein")
+  void post_createProfil_backEnd_idNeedsToBeInt() throws Exception {
+    mockMvc.perform(post("/admin")
+        .param("id", "0")
+        .param("name", "test")
+        .with(csrf()));
+    verify(editor, never()).create(anyInt(), any());
+  }
+  
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  @DisplayName("name darf nicht leer sein")
+  void post_createProfil_backEnd_NameCantBeEmpty() throws Exception {
+    mockMvc.perform(post("/admin")
+        .param("id", "1")
+        .param("name", "")
+        .with(csrf()));
+    verify(editor, never()).create(anyInt(), any());
+  }
 }
