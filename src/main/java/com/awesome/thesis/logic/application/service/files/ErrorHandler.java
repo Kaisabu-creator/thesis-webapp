b9a73999f4a6740d0e181665f481bdfa5b674d12
Ole Marschik <ole.marschik@hhu.de>
2026-01-21 14:32:25 +0100

    checkstyle fixed ErrorHandler

--- src/main/java/com/awesome/thesis/logic/application/service/files/ErrorHandler.java
+++ src/main/java/com/awesome/thesis/logic/application/service/files/ErrorHandler.java
@@ -7,12 +7,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+/**
+ * Klasse zur Behandlung von eigenen Fehlern.
+ */
 @ControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleException(MaxUploadSizeExceededException maxUploadSizeExceededExceptionException, Model model) {
-        model.addAttribute("maxUploadError","Maximale Dateigröße überschritten: max. 10MB");
-        return "upload";
-    }}
+  /**
+   * Methode zur Behandlung von MaxUploadSizeExceededException.
+   *
+   * @param maxUploadSizeExceededExceptionException Exception für zu große Dateien.
+   * @param model                                   Model-Attribut für Fehlermeldung.
+   * @return Gibt die upload.html zurück.
+   */
+  @ExceptionHandler(MaxUploadSizeExceededException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String handleException(
+      MaxUploadSizeExceededException maxUploadSizeExceededExceptionException,
+      Model model) {
+    model.addAttribute("maxUploadError", "Maximale Dateigröße überschritten: max. 10MB");
+    return "upload";
+  }
+}
