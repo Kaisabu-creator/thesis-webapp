802134d3b689cc5eb2ec871d8320889e499eec76
Janik Daub <daub.j@icloud.com>
2026-01-21 13:18:33 +0100

    Controller follows Google Standard

--- src/main/java/com/awesome/thesis/controller/betreuende/BetreuendeProfilEditController.java
+++ src/main/java/com/awesome/thesis/controller/betreuende/BetreuendeProfilEditController.java
@@ -15,114 +15,199 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller für die Bearbeitung eines Betreuendenprofils.
+ */
 @Controller
 @RequestMapping("/betreuende")
 @Secured("ROLE_BETREUENDE")
 public class BetreuendeProfilEditController {
-    @Autowired
-    ProfilEditor editor;
-
-    @GetMapping("profilEdit")
-    public String profilEdit(Model model, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        model.addAttribute("profil", editor.get(id));
-        model.addAttribute("kontakt", new EmailKontaktDto("email","", ""));
-        model.addAttribute("linkDTO", new LinkDTO("", ""));
-        model.addAttribute("profilEditDTO", new ProfilEditDto(""));
-        model.addAttribute("fachgebietDTO", new FachgebietDto(""));
-        return "betreuende/profilEdit";
+  @Autowired
+  ProfilEditor editor;
+  
+  /**
+   * Methode für das Get-Mapping auf profilEdit.
+   *
+   * @param model {@link Model}
+   * @param auth  {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return profilEdit.html Template mit Profil Daten
+   */
+  @GetMapping("profilEdit")
+  public String profilEdit(Model model, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    model.addAttribute("profil", editor.get(id));
+    model.addAttribute("kontakt", new EmailKontaktDto("email", "", ""));
+    model.addAttribute("linkDTO", new LinkDTO("", ""));
+    model.addAttribute("profilEditDTO", new ProfilEditDto(""));
+    model.addAttribute("fachgebietDTO", new FachgebietDto(""));
+    return "betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für das Post-Mapping der Namensänderung.
+   *
+   * @param profilEditDto {@link ProfilEditDto} um neue Nutzerdaten zu erhalten
+   * @param result        {@link BindingResult} um mit fehlschlagender Validierung umzugehen
+   * @param model         {@link Model}
+   * @param auth          {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf betreuende/profilEdit und bei Fehlern im BindingResult profilEdit.html
+   */
+  @PostMapping("profilEdit")
+  public String profilEdit(@Valid @ModelAttribute() ProfilEditDto profilEditDto,
+                           BindingResult result, Model model, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    if (result.hasErrors()) {
+      model.addAttribute("profil", editor.get(id));
+      model.addAttribute("kontakt", new EmailKontaktDto("email", "", ""));
+      model.addAttribute("linkDTO", new LinkDTO("", ""));
+      model.addAttribute("fachgebietDTO", new FachgebietDto(""));
+      return "betreuende/profilEdit";
     }
-
-    @PostMapping("profilEdit")
-    public String profilEdit(@Valid @ModelAttribute() ProfilEditDto profilEditDTO, BindingResult result, Model model, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        if  (result.hasErrors()) {
-            model.addAttribute("profil", editor.get(id));
-            model.addAttribute("kontakt", new EmailKontaktDto("email","", ""));
-            model.addAttribute("linkDTO", new LinkDTO("", ""));
-            model.addAttribute("fachgebietDTO", new FachgebietDto(""));
-            return "betreuende/profilEdit";
-        }
-        editor.editName(id, profilEditDTO.name());
-        return "redirect:/betreuende/profilEdit";
+    editor.editName(id, profilEditDto.name());
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für post-Mapping um Kontakt zu löschen.
+   *
+   * @param profilKontakt {@link ProfilKontakt} um Kontakt zu laden
+   * @param auth          {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf /betreuende/profilEdit
+   */
+  @PostMapping("profilEdit/deleteKontakt")
+  public String deleteKontakt(@ModelAttribute ProfilKontakt profilKontakt,
+                              OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    editor.removeKontakt(id, profilKontakt);
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für Post-Mapping um eine E-Mail hinzuzufügen.
+   *
+   * @param email  {@link EmailKontaktDto} zur Validierung
+   * @param result {@link BindingResult} um mit fehlschlagender Validierung umzugehen
+   * @param model  {@link Model}
+   * @param auth   {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf betreuende/profilEdit und bei Fehlern im BindingResult profilEdit.html
+   */
+  @PostMapping(value = "profilEdit/addKontakt", params = "type=email")
+  public String addEmail(@Valid @ModelAttribute("kontakt") EmailKontaktDto email,
+                         BindingResult result, Model model, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    if (result.hasErrors()) {
+      model.addAttribute("profil", editor.get(id));
+      model.addAttribute("linkDTO", new LinkDTO("", ""));
+      model.addAttribute("profilEditDTO", new ProfilEditDto(""));
+      model.addAttribute("fachgebietDTO", new FachgebietDto(""));
+      return "betreuende/profilEdit";
     }
-
-    @PostMapping("profilEdit/deleteKontakt")
-    public String deleteKontakt(@ModelAttribute ProfilKontakt profilKontakt, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        editor.removeKontakt(id, profilKontakt);
-        return "redirect:/betreuende/profilEdit";
+    editor.addEmail(id, email.label(), email.wert());
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für Post-Mapping um eine E-Mail hinzuzufügen.
+   *
+   * @param tel    {@link TelKontaktDto} zur Validierung
+   * @param result {@link BindingResult} um mit fehlschlagender Validierung umzugehen
+   * @param model  {@link Model}
+   * @param auth   {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf betreuende/profilEdit und bei Fehlern im BindingResult profilEdit.html
+   */
+  @PostMapping(value = "profilEdit/addKontakt", params = "type=tel")
+  public String addTel(@Valid @ModelAttribute("kontakt") TelKontaktDto tel, BindingResult result,
+                       Model model, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    if (result.hasErrors()) {
+      model.addAttribute("profil", editor.get(id));
+      model.addAttribute("linkDTO", new LinkDTO("", ""));
+      model.addAttribute("profilEditDTO", new ProfilEditDto(""));
+      model.addAttribute("fachgebietDTO", new FachgebietDto(""));
+      return "betreuende/profilEdit";
     }
-
-    @PostMapping(value="profilEdit/addKontakt", params="type=email")
-    public String addEmail(@Valid @ModelAttribute("kontakt") EmailKontaktDto email, BindingResult result, Model model, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        if  (result.hasErrors()) {
-            model.addAttribute("profil", editor.get(id));
-            model.addAttribute("linkDTO", new LinkDTO("", ""));
-            model.addAttribute("profilEditDTO", new ProfilEditDto(""));
-            model.addAttribute("fachgebietDTO", new FachgebietDto(""));
-            return "betreuende/profilEdit";
-        }
-        editor.addEmail(id, email.label(), email.wert());
-        return "redirect:/betreuende/profilEdit";
+    editor.addTel(id, tel.label(), tel.wert());
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für Post-Mapping zum Hinzufügen von einem Fachgebiet.
+   *
+   * @param fachgebietDto {@link FachgebietDto} zur Validierung
+   * @param result        {@link BindingResult} um mit fehlschlagender Validierung umzugehen
+   * @param model         {@link Model}
+   * @param auth          {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf betreuende/profilEdit und bei Fehlern im BindingResult profilEdit.html
+   */
+  @PostMapping("profilEdit/addFachgebiet")
+  public String addFachgebiet(@Valid @ModelAttribute() FachgebietDto fachgebietDto,
+                              BindingResult result, Model model, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    if (result.hasErrors()) {
+      model.addAttribute("profil", editor.get(id));
+      model.addAttribute("kontakt", new EmailKontaktDto("email", "", ""));
+      model.addAttribute("linkDTO", new LinkDTO("", ""));
+      model.addAttribute("profilEditDTO", new ProfilEditDto(""));
+      return "betreuende/profilEdit";
     }
-
-    @PostMapping(value="profilEdit/addKontakt", params="type=tel")
-    public String addTel(@Valid @ModelAttribute("kontakt") TelKontaktDto tel, BindingResult result, Model model, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        if  (result.hasErrors()) {
-            model.addAttribute("profil", editor.get(id));
-            model.addAttribute("linkDTO", new LinkDTO("", ""));
-            model.addAttribute("profilEditDTO", new ProfilEditDto(""));
-            model.addAttribute("fachgebietDTO", new FachgebietDto(""));
-            return "betreuende/profilEdit";
-        }
-        editor.addTel(id, tel.label(), tel.wert());
-        return "redirect:/betreuende/profilEdit";
+    editor.addFachgebiet(id, fachgebietDto.fachgebiet());
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für Post-Mapping zum Löschen von Fachgebieten.
+   *
+   * @param fachgebiet Name von Fachgebiet
+   * @param auth {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf betreuende/profilEdit
+   */
+  @PostMapping("profilEdit/removeFachgebiet")
+  public String removeFachgebiet(String fachgebiet, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    editor.removeFachgebiet(id, fachgebiet);
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für Post-Mapping um einen Link hinzuzufügen.
+   *
+   * @param linkDto {@link LinkDTO} zur Validierung
+   * @param result {@link BindingResult} um mit fehlschlagender Validierung umzugehen
+   * @param model  {@link Model}
+   * @param auth   {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf betreuende/profilEdit und bei Fehlern im BindingResult profilEdit.html
+   */
+  @PostMapping("/profilEdit/addLink")
+  public String addLink(@Valid @ModelAttribute() LinkDTO linkDto, BindingResult result,
+                        Model model, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    if (result.hasErrors()) {
+      model.addAttribute("profil", editor.get(id));
+      model.addAttribute("kontakt", new EmailKontaktDto("email", "", ""));
+      model.addAttribute("profilEditDTO", new ProfilEditDto(""));
+      model.addAttribute("fachgebietDTO", new FachgebietDto(""));
+      return "betreuende/profilEdit";
     }
-
-    @PostMapping("profilEdit/addFachgebiet")
-    public String addFachgebiet(@Valid @ModelAttribute() FachgebietDto fachgebietDTO, BindingResult result, Model model, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        if (result.hasErrors()) {
-            model.addAttribute("profil", editor.get(id));
-            model.addAttribute("kontakt", new EmailKontaktDto("email","", ""));
-            model.addAttribute("linkDTO", new LinkDTO("", ""));
-            model.addAttribute("profilEditDTO", new ProfilEditDto(""));
-            return "betreuende/profilEdit";
-        }
-        editor.addFachgebiet(id, fachgebietDTO.fachgebiet());
-        return "redirect:/betreuende/profilEdit";
-    }
-
-    @PostMapping("profilEdit/removeFachgebiet")
-    public String removeFachgebiet(String fachgebiet, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        editor.removeFachgebiet(id, fachgebiet);
-        return "redirect:/betreuende/profilEdit";
-    }
-
-    @PostMapping("/profilEdit/addLink")
-    public String editThemaLink(@Valid @ModelAttribute() LinkDTO linkDTO, BindingResult result, Model model, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        if (result.hasErrors()){
-            model.addAttribute("profil", editor.get(id));
-            model.addAttribute("kontakt", new EmailKontaktDto("email","", ""));
-            model.addAttribute("profilEditDTO", new ProfilEditDto(""));
-            model.addAttribute("fachgebietDTO", new FachgebietDto(""));
-            return "betreuende/profilEdit";
-        }
-        editor.addLink(id, linkDTO.url(), linkDTO.urlBeschreibung());
-        return "redirect:/betreuende/profilEdit";
-    }
-
-    @PostMapping("/profilEdit/deleteLink")
-    public String deleteLink(@ModelAttribute ProfilLink link, OAuth2AuthenticationToken auth) {
-        Integer id = auth.getPrincipal().getAttribute("id");
-        editor.removeLink(id, link);
-        return "redirect:/betreuende/profilEdit";
-    }
+    editor.addLink(id, linkDto.url(), linkDto.urlBeschreibung());
+    return "redirect:/betreuende/profilEdit";
+  }
+  
+  /**
+   * Methode für Post-Mapping zum Löschen eines Links.
+   *
+   * @param link {@link ProfilLink}
+   * @param auth {@link OAuth2AuthenticationToken} um auf Github-Id zuzugreifen
+   * @return redirect auf /betreuende/profilEdit
+   */
+  @PostMapping("/profilEdit/deleteLink")
+  public String deleteLink(@ModelAttribute ProfilLink link, OAuth2AuthenticationToken auth) {
+    Integer id = auth.getPrincipal().getAttribute("id");
+    editor.removeLink(id, link);
+    return "redirect:/betreuende/profilEdit";
+  }
 }
