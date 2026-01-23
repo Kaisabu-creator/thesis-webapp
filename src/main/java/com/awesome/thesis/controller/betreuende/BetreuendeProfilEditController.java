e45858eea56f3552742bf273ebbbafd04d4e0e09
Janik Daub <daub.j@icloud.com>
2026-01-23 09:25:29 +0100

    Tests for Matching Controller

--- src/main/java/com/awesome/thesis/controller/betreuende/BetreuendeProfilEditController.java
+++ src/main/java/com/awesome/thesis/controller/betreuende/BetreuendeProfilEditController.java
@@ -18,7 +18,11 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
--- src/test/java/com/awesome/thesis/controller/MatchingControllerTest.java
+++ src/test/java/com/awesome/thesis/controller/MatchingControllerTest.java
@@ -1,7 +1,10 @@
 package com.awesome.thesis.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.awesome.thesis.configurations.AppUserService;
 import com.awesome.thesis.configurations.MethodSecurityConfig;
@@ -11,6 +14,10 @@ import com.awesome.thesis.logic.application.service.fachgebiete.FachgebieteEdito
 import com.awesome.thesis.logic.application.service.profiles.ProfilEditor;
 import com.awesome.thesis.logic.application.service.themen.ThemaEditor;
 import com.awesome.thesis.logic.application.service.voraussetzungen.VoraussetzungenEditor;
+import com.awesome.thesis.logic.domain.model.profil.Profil;
+import com.awesome.thesis.logic.domain.model.themen.Thema;
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,9 +46,58 @@ class MatchingControllerTest {
   
   @Test
   @WithMockOAuth2User()
-  @DisplayName("get auf /matching")
-  void test_getProfilId() throws Exception {
+  @DisplayName("get auf /matching ohne Parametern ist ok")
+  void test_getMatching() throws Exception {
+    //Act + Assert
     mockMvc.perform(get("/matching"))
         .andExpect(status().isOk());
+  }
+  
+  @Test
+  @WithMockOAuth2User()
+  @DisplayName("get auf /matching mit Parametern ist ok")
+  void test_getMatchingParam() throws Exception {
+    //Act + Assert
+    mockMvc.perform(get("/matching")
+            .param("interessen", "eingabe1")
+            .param("voraussetzungen", "eingabe2"))
+        .andExpect(status().isOk());
+  }
+  
+  @Test
+  @WithMockOAuth2User()
+  @DisplayName("get auf /matching gibt richtige View zurück")
+  void test_getMatchingView() throws Exception {
+    //Act + Assert
+    mockMvc.perform(get("/matching"))
+        .andExpect(view().name("matching"));
+  }
+  
+  @Test
+  @WithMockOAuth2User()
+  @DisplayName("get auf /matching has right model")
+  void test_getMatchingModel() throws Exception {
+    //Arrange
+    Set<String> interessen = Set.of("eingabe1");
+    Set<String> voraussetzungen = Set.of("eingabe2");
+    List<Profil> p = List.of(new Profil(1, "test"));
+    when(profilEditor.getMatching(interessen)).thenReturn(p);
+    List<Thema> t = List.of(new Thema("test", 1));
+    when(themaEditor.sortRang(voraussetzungen, interessen)).thenReturn(t);
+    Set<String> f = Set.of("fachgebiet");
+    when(fachgebieteEditor.getAll()).thenReturn(f);
+    Set<String> v = Set.of("voraussetzung");
+    when(vorEditor.getAllString()).thenReturn(v);
+    
+    //Act + Assert
+    mockMvc.perform(get("/matching")
+            .param("interessen", "eingabe1")
+            .param("voraussetzungen", "eingabe2"))
+        .andExpect(model().attribute("fachgebiete", f))
+        .andExpect(model().attribute("voraussetzungen", v))
+        .andExpect(model().attribute("profile", p))
+        .andExpect(model().attribute("themenListe", t))
+        .andExpect(model().attribute("interessen", Set.of("eingabe1")))
+        .andExpect(model().attribute("eingabeVoraussetzungen", Set.of("eingabe2")));
   }
 }
